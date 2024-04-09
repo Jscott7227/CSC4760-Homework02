3b7d8d433c98aa6c2b3b642c693c573079e82796
Jared Scott <150862732+Jscott7227@users.noreply.github.com>
2024-04-09 12:16:00 -0500

    Update Bcast_Reduce.cpp

--- example/homework/04/Bcast_Reduce.cpp
+++ example/homework/04/Bcast_Reduce.cpp
@@ -20,9 +20,9 @@ int main(int argc, char **argv)
        sendbuf[i] = (float)rank;
 
     //Use reduce to take all elements from other ranks and convert them into a single value on the root rank 0
-    MPI_Reduce(sendbuf, recvbuf, 5, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&sendbuf, &recvbuf, 5, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
     //Send the value in recvbuf on the root rank to all other ranks
-    MPI_Bcast(recvbuf, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&recvbuf, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     //Print value of recvbuf for all ranks
     cout << rank << ": ";
